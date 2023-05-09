15588de595fb1d4e0d810b08386a6b666064c315
eulauragabriel <eulauragabriel@gmail.com>
2023-05-09 08:17:05 -0300

    alterando o meu exercício 17

--- python/exercicio_17.py
+++ python/exercicio_17.py
@@ -3,4 +3,4 @@ import math
 co = int(input('digite o cateto oposto: '))
 ca = int(input('digite o cateto adjacente: '))
 h = co**2 + ca**2
-print(math.sqrt(h))
+print(f'a hipotenusa é {math.sqrt(h)}')
